5ed2fe51d6d084e17df3fd7f1439cb11ee994775
Gledi Caushaj <gledi.alb@gmail.com>
2022-08-21 12:17:45 +0200

    Add admin for movies models

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,28 @@
+import datetime as dt
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Movie, Actor, Director
+
+
+admin.site.register(Actor)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'year', 'runtime', 'rating']
+    list_filter = ['rating']
+    list_editable = ['year', 'runtime']
+
+
+admin.site.register(Movie, MovieAdmin)
+
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'birthday']
+
+    @admin.display(empty_value='-')
+    def birthday(self, obj):
+        today = dt.date.today()
+        if obj.birthdate.day == today.day and obj.birthdate.month == today.month:
+            return 'Happy Birthday'
+        return obj.birthdate
